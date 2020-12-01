9976d3ba0672892233fa398b8970f91d546c78f0
Adam Wolcott Smith <adamwolcottsmith@gmail.com>
2020-11-30 19:27:15 -0500

    made cart html

--- frontend/product.js
+++ frontend/product.js
@@ -4,7 +4,7 @@ const closeCartBtn = document.querySelector('.close-cart');
 const clearCartBtn = document.querySelector('.clear-cart');
 const cartDOM = document.querySelector('.cart');
 const cartOverlay = document.querySelector('.cart-overlay');
-const cartItems = document.querySelector('.cart-items');
+const cartItems = document.querySelector('.cart-item');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
@@ -12,6 +12,8 @@ const productsDOM = document.querySelector('.products-center');
 const apiUrl = 'http://localhost:3000/api/teddies/';
 const urlParams = new URLSearchParams(window.location.search);
 const productId = urlParams.get('id');
+
+
 
 //cart
 let cart = [];
@@ -52,8 +54,8 @@ class UI {
       <select name="colors" class="color-choice">
         ${item.colors.map(color => { return `<option class="color-options" value="${color}">${color}</option>` }).join("")}
       </select>
-      <input type="number" class="product-amt" min="0">
-      <h4>$${item.price}</h4>
+      <input type="number" class="product-amt" min="0" placeholder="0">
+      <h4 class="item-price">$${item.price}</h4>
       <button class="bag-btn-out" data-id=${item.id}>
          <i class="fas fa-shopping-cart"></i>
          add to bag
@@ -91,17 +93,40 @@ class UI {
       let cartQuantity = quantity.value
       console.log(cartQuantity);
 
+      //price 
+      let price = document.querySelector('.item-price').textContent;
+      price = parseInt(price.slice(1), 10);
+      console.log(price);
+
       //storage
       let products = [];
       if (localStorage.getItem('products')) {
         products = JSON.parse(localStorage.getItem('products'));
       }
-      products.push({ 'productId': id, colorChoice, cartQuantity });
+      products.push({ 'productId': id, colorChoice, cartQuantity, price });
       localStorage.setItem('products', JSON.stringify(products));
 
+      //cart
+      this.setCartValues(products)
+    });
+  }
+  setCartValues(products) {
+    if (products.length > 0) {
+      let navIcon = document.querySelector('.cart-icon');
+      navIcon.innerText = products.length
+    }
 
-
-    })
+    // console.log('setting cart values');
+    // console.log(products);
+    // let tempTotal = 0;
+    // let itemsTotal = 0;
+    // products.map(item => {
+    //   tempTotal += item.price * item.amount;
+    //   itemsTotal += item.amount
+    // })
+    // cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    // cartItems.innerText = itemsTotal;
+    // console.log(cartTotal);
   }
 };
 
@@ -111,25 +136,37 @@ class Storage {
   static saveProduct(id, color) {
     localStorage.setItem('cart', JSON.stringify(id, color));
   };
-  // static getProduct(id) {
-  //  let products = JSON.parse(localStorage.getItem('products'));
-  //  // return products.find(product => product._id === id);
-  // };
+  static getProduct(id) {
 
+  };
 
   static saveCart(cart) {
     localStorage.setItem('cart', JSON.stringify(cart));
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const ui = new UI();
-  const product = new Product();
+// document.addEventListener('DOMContentLoaded', () => {
+//   const ui = new UI();
+//   const product = new Product();
 
-  //get product
-  product.getProduct().then(product => {
-    ui.displayProduct(product);
-  }).then(() => {
-    ui.getBagButton();
-  });
+//   //get product
+//   product.getProduct().then(product => {
+//     ui.displayProduct(product);
+//   }).then(() => {
+//     ui.getBagButton();
+//   });
+// });
+
+const ui = new UI();
+const product = new Product();
+
+console.log('first', product);
+
+//get product
+product.getProduct().then(product => {
+  ui.displayProduct(product);
+}).then(() => {
+  ui.getBagButton();
 });
+
+console.log('second', product);
