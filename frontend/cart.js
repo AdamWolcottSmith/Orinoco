3acd3771575884a230543dd7d83a8aca6ca5d38f
Adam Wolcott Smith <adamwolcottsmith@gmail.com>
2020-12-02 18:08:17 -0500

    fixed chevrons and created total calc for cart page

--- frontend/cart.js
+++ frontend/cart.js
@@ -5,49 +5,105 @@ const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
 
+const urlString = './product.html?id=';
+
 function addCartItem(item) {
   const div = document.createElement('div')
   div.classList.add('cart-item')
   div.innerHTML = `
+    <a href="${urlString + item.productId}">
       <img src="${item.image}" alt="product">
-          <div>
-            <h4>${item.name}</h4>
-            <h5>$${item.price}</h5>
-            <span class="remove-item" data-id=${item.id}>remove</span>
-          </div>
-          <div>
-            <i class="fas fa-chevron-up" data-id=${item.productId}></i>
-            <p class="item-amount">${item.cartQuantity}</p>
-            <i class="fas fa-chevron-down" data-id=${item.productId}></i>
-          </div>
- `;
+    </a>
+    <div>
+      <h4>${item.name}</h4>
+      <h6>${item.colorChoice}</h6>
+      <h5>$${item.price}</h5>
+      <span class="remove-item" data-id=${item.productId}>remove</span>
+    </div>
+    <div>
+      <i class="fas fa-chevron-up" data-id=${item.productId}></i>
+      <p class="item-amount">${item.cartQuantity}</p>
+      <i class="fas fa-chevron-down" data-id=${item.productId}></i>
+    </div>
+  `;
   cartContent.appendChild(div)
 }
 
 if (localStorage.getItem('products') == null) {
   const div = document.createElement('div')
   div.innerHTML = `
-      <p style="text-align: center" >There is nothing in your cart</p>
- `;
+    <p style="text-align: center" >There is nothing in your cart</p>
+    `;
   cartContent.appendChild(div)
 } else {
+
   const data = JSON.parse(localStorage.getItem('products'))
+
   for (let i = 0; i < data.length; i++) {
     addCartItem(data[i])
   }
-  const chevUp = document.querySelector('.fa-chevron-up');
-  const chevDown = document.querySelector('.fa-chevron-down');
-  chevUp.addEventListener('click', () => {
-    console.log('Qty Up');
-    let products = data.filter(product => (chevUp.dataset.id === product.productId))
-    let product = products[0]
-    product.cartQuantity += 1
-    //fix numbering system
-    console.log(data);
+
+
+
+  const chevUpElems = document.querySelectorAll('.fa-chevron-up');
+  const chevDownElems = document.querySelectorAll('.fa-chevron-down');
+  const removeElem = document.querySelectorAll('.remove-item');
+  const cartTotalElem = document.querySelector('.cart-total');
+
+  // CART TOTAL CALC
+
+  function cartTotalSum() {
+    const cartItem = data.filter(product => product.productId);
+    const itemSum = cartItem.map(product => product.cartQuantity * product.price);
+    const cartTotal = itemSum.reduce((acc, item) => acc + item, 0);
+    cartTotalElem.innerText = cartTotal;
+  }
+  cartTotalSum();
+
+  // QTY INCREMENT
+
+  chevUpElems.forEach(chevUp => {
+    chevUp.addEventListener('click', (e) => {
+      let products = data.filter(product => (e.target.dataset.id === product.productId));
+      let product = products[0];
+      product.cartQuantity++;
+      e.target.nextElementSibling.innerText = product.cartQuantity;
+      localStorage.setItem('products', JSON.stringify(data))
+      cartTotalSum()
+    })
   })
-  chevDown.addEventListener('click', () => {
-    console.log('Qty Down');
+  chevDownElems.forEach(chevDown => {
+    chevDown.addEventListener('click', (e) => {
+      let products = data.filter(product => (e.target.dataset.id === product.productId));
+      let product = products[0];
+      product.cartQuantity--;
+      e.target.previousElementSibling.innerText = product.cartQuantity;
+      localStorage.setItem('products', JSON.stringify(data))
+      cartTotalSum()
+    })
   })
+
+  // console.log(removeElem);
+
+  // removeElem.forEach(removeBtn => {
+  //   removeBtn.addEventListener('click', (e) => {
+  //     let products = data.filter(product => (e.target.dataset.id === product.productId));
+  //     let product = products[0];
+  //     for (let i = 0; i < data.length; i++) {
+  //       if (data[i].productId === product) {
+  //         data.splice(i, 1);
+  //         break;
+  //       }
+  //       localStorage.setItem('products', JSON.stringify(data))
+  //     }
+
+  //     console.log(data);
+  //   })
+  // })
+
+
+
+
 }
 
 
